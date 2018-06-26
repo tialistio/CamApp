635ebdd47b8da10f0a39d186ae663bc80285cc69
tialistio <tia.listio@gmail.com>
2018-06-26 16:01:49 +0900

    Main menu created

--- app/src/main/java/com/example/a1/camapp/MainActivity.java
+++ app/src/main/java/com/example/a1/camapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.a1.camapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
+
+        ImageButton camera = (ImageButton)findViewById(R.id.ib_camera);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, OpenCamera.class));
+            }
+        });
+
     }
 }
